fcf240496a93d6e1bb9b6ce60626fa48323fb7ec
Andrew Binkowski <andrew@babybinks.com>
2011-10-20 09:37:34 -0500

    Initial Commit

---
+++ Lecture 4 - Demonstration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lecture 4 - Demonstration
+//
+//  Created by T. Andrew Binkowski on 10/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lecture 4 - Demonstration/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lecture 4 - Demonstration
+//
+//  Created by T. Andrew Binkowski on 10/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
